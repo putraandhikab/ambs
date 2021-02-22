f791424bc8d81f52f1650019af817a7276dee6a5
putraandhikab <57262200+putraandhikab@users.noreply.github.com>
2021-02-22 22:25:25 +0700

    tampilan awal tinggal finishing & nambah home setelah login

---
+++ ambs/homelogin.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/homelogin.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="container">
+            <a class="navbar-brand-dark" href="#">AMBS</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mr-3">
+                    <li class="nav-item active mr-3">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active mr-3">
+                        <a class="nav-link" href="#">Library <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active mr-3">
+                        <a class="nav-link" href="#">My Notes <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active mr-3">
+                        <a class="nav-link" href="#">Profile <span class="sr-only">(current)</span></a>
+                    </li>
+                    
+                </ul>
+                <button class="btn btn-danger my-2 my-sm-0" type="submit">Log Out</button>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Recent -->
+    <div class="text-center">
+        <h2 class="section-heading mt-4">Recent Notes</h2><br><br>
+    </div>
+
+    <!-- File -->
+    <div class="card ml-5" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title text-center">Catatan Biologi</h5>
+            <a href="#" class="btn btn-primary">View</a>
+        </div>
+    </div>
+    <div class="card ml-5" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title text-center">Catatan Biologi</h5>
+            <a href="#" class="btn btn-primary">View</a>
+        </div>
+    </div>
+    <div class="card ml-5" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title text-center">Catatan Biologi</h5>
+            <a href="#" class="btn btn-primary">View</a>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
